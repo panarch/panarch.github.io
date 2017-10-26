680e8f6091c590d90dfdd28ac34fbf0ae4879641
Taehoon Moon <panarch@kaist.ac.kr>
2017-10-27 05:05:44 +0900

    Finish CTP431 2017 HW2

--- ctp431/hw2/index.js
+++ ctp431/hw2/index.js
@@ -1,92 +1,98 @@
+const $body = document.body;
+const $select = document.querySelector('select');
+const $file = document.querySelector('input[type="file"]');
+const $container = document.querySelector('.container');
 const $box = document.querySelector('.box');
 const $bars = document.querySelectorAll('.box > div');
+const $micButton = document.querySelector('.mic-button');
+const $demoButton = document.querySelector('.demo-button');
+const $fileButton = document.querySelector('.file-button');
+$micButton.addEventListener('click', playMic, false);
+$demoButton.addEventListener('click', playFile, false);
+$fileButton.addEventListener('click', playFile.bind(null, false), false);
+$select.addEventListener('change', loadSample);
+$file.addEventListener('change', loadFile);
+
+const HEIGHT = 400;
+const MIN_DB = -96.0; // dB
 
 const context = new AudioContext();
 const analyser = context.createAnalyser();
 analyser.fftSize = 2048;
 analyser.smoothingTimeConstant = 0;    
 
-var source = null;
-var myAudioBuffer = null;
-var sourceNode = null;
-var mediaSourceNode = null;
+let source = null;
+let sourceNode = null;
+let mediaSourceNode = null;
 
-var HEIGHT = 400;
-var SOUND_METER_MIN_LEVEL = -96.0;  // dB
+let micOn = false;
+let filePlayOn = false;
+let animationId;
+let demoBuffer;
+let fileBuffer;
 
-var micOn = false;
-var filePlayOn = false;
-
-var animation_function;
-var animation_id;
-
-// load demo audio feils
-var demo_buffer;
-
-const $micAudio = document.querySelector('#micInput');
-const $demoAudio = document.querySelector('#demoAudio');
-$micAudio.addEventListener("click", playMic, false);
-$demoAudio.addEventListener("click", playFile, false);
-
-window.onload= async function(){
-  
-  // analyzer
-
-  const fileName = 'Piano_Store.mp3';
+async function loadSample() {
+  $demoButton.setAttribute('disabled', '');
+  const fileName = $select.value;
   const response = await fetch(`./music/${ fileName }`, { method: 'GET' });
   const result = await response.arrayBuffer();
-  context.decodeAudioData(result, function (buffer) { demo_buffer = buffer; });
-  animation_function = draw_octaveband;
+  demoBuffer = await context.decodeAudioData(result);
+
+  $demoButton.removeAttribute('disabled');
 }
 
+async function loadFile() {
+  $fileButton.setAttribute('disabled', '');
+  const file = $file.files[0];
+  const fileReader = new FileReader();
+  fileReader.onload = async function (event) {
+    fileBuffer = await context.decodeAudioData(event.target.result);
+    $fileButton.removeAttribute('disabled');
+  };
+  fileReader.readAsArrayBuffer(file);
+
+}
+
+loadSample();
+
+function getColor(color) { return `rgb(${ color }, ${ color }, ${ color })`; }
+
 let prevTime = null;
+let prevSoundLevels = null;
 let angle = 0;
 let scale = 1.0;
-let prevSoundLevels = null;
-let rotateDirection = 1;
 let origin = 0.5; // 0 ~ 1.0
 let originDirection = 1;
-function draw_octaveband(time) {
+let rotateDirection = 1;
 
-  // get samples 
-  var data_array = new Float32Array(analyser.frequencyBinCount);
-  analyser.getFloatFrequencyData(data_array);
+function animate(time) {
+  const dataArray = new Float32Array(analyser.frequencyBinCount);
+  analyser.getFloatFrequencyData(dataArray);
 
-  var octaveband_level_db = calc_octaveband(data_array)
-
-  function _calculateSoundLevel(db) {
-    return (db - SOUND_METER_MIN_LEVEL)/(0.0-SOUND_METER_MIN_LEVEL)*HEIGHT;
-  }
-
-  const soundLevels = octaveband_level_db.map(db => _calculateSoundLevel(db))
-
+  const dbs = calcOctaveband(dataArray)
+  const soundLevels = dbs.map(db => ((db - MIN_DB) / (0.0 - MIN_DB) * HEIGHT));
   const color = (255 - soundLevels[0] * 35 / HEIGHT) | 0;
-  document.body.style.backgroundColor = `rgb(${ color }, ${ color }, ${ color })`;
-
   const color2 = (255 - soundLevels[1] * 25 / HEIGHT) | 0;
-  document.querySelector('.container').style.backgroundColor = `rgb(${ color2 }, ${ color2 }, ${ color2 })`;
-
   const color3 = (255 - soundLevels[2] * 15 / HEIGHT) | 0;
-  $box.style.backgroundColor = `rgb(${ color3 }, ${ color3 }, ${ color3 })`;
-
-  // 2d canvas context
+  $body.style.backgroundColor = getColor(color);
+  $container.style.backgroundColor = getColor(color2);
+  $box.style.backgroundColor = getColor(color3);
 
   const scaleX = 0.4 + soundLevels[5] / 350;
 
-  for (var i=0; i<10; i++) {
-		const $bar = $bars[i];
-		$bar.textContent = octaveband_level_db[i] | 0;
-
-    // fill rectangular (for the sound level)
+  for (let i = 0; i < 10; i++) {
+    const $bar = $bars[i];
     const soundLevel = soundLevels[i];
     const scaleY = soundLevel / HEIGHT * 1.2;
     const translateY = origin * 400 + ((0.5 - origin) * 10) - origin * scaleY * HEIGHT;
     const transform = 
       `translate3d(${ 39 * i + 9 }px, ${ translateY }px, 0) scale3d(${ scaleX }, ${ scaleY }, 1.0)`;
+
     $bar.style.transform = transform; 
+    $bar.textContent = dbs[i] | 0;
   }
 
-  const _velocity = -soundLevels[3] / 8;
+  const _rotateVelocity = -soundLevels[3] / 8;
   const _scaleVelocity = soundLevels[4] / 300;
   const _originVelocity = soundLevels[8] / 300;
 
@@ -96,7 +102,7 @@ function draw_octaveband(time) {
       rotateDirection *= -1;
     }
     const gap = (time - prevTime) / 1000;
-    angle += gap * _velocity * rotateDirection;
+    angle += gap * _rotateVelocity * rotateDirection;
     scale = 0.6 + soundLevels[5] / 400;
 
     origin += gap * _originVelocity * originDirection;
@@ -113,50 +119,49 @@ function draw_octaveband(time) {
   } else {
     $box.style.opacity = 1.0;
   }
+
   prevTime = time;
   prevSoundLevels = soundLevels;
 
-  animation_id = requestAnimationFrame(animation_function);
+  animationId = requestAnimationFrame(animate);
 }
 
-function playMic()
-{
-    if (filePlayOn) {
-      turnOffFileAudio();
-    }
+function playMic() {
+  if (filePlayOn) {
+    turnOffFileAudio();
+  }
 
-    if (micOn) {
+  if (micOn) {
     turnOffMicAudio();
     return;
-    }
+  }
+
+  if (!navigator.getUserMedia) {
+    navigator.getUserMedia = (navigator.getUserMedia({audio: true}) || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  }
 
   if (!navigator.getUserMedia)
-    navigator.getUserMedia = (navigator.getUserMedia({audio: true}) || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-                
-  if (!navigator.getUserMedia)
-    alert("Error: getUserMedia not supported!");
-            
-  // get audio input streaming          
-  navigator.getUserMedia({audio: true}, onStream, onStreamError)
+    alert('Error: getUserMedia not supported!');
+
+  // get audio input streaming
+  navigator.getUserMedia({ audio: true }, onStream, onStreamError);
 
   micOn = true;
-
-  var mic = document.getElementById("micInput");
-  mic.innerHTML = 'Mic Off'
+  $micButton.textContent = 'Mic Off';
 }
 
 let _stream;
 
 // success callback
 function onStream(stream) {
-    mediaSourceNode = context.createMediaStreamSource(stream);
-    _stream = stream;
-  
+  mediaSourceNode = context.createMediaStreamSource(stream);
+  _stream = stream;
+
   // Connect graph
   mediaSourceNode.connect(analyser);
-              
+
   // visualize audio animation
-  animation_id = requestAnimationFrame(animation_function, context.sampleRate/analyser.fftSize);
+  animationId = requestAnimationFrame(animate, context.sampleRate / analyser.fftSize);
 }
 
 // errorCallback       
@@ -166,38 +171,36 @@ function onStreamError(error) {
   micOn = false;
 }
 
+function playFile(demo = true) {
+  if (filePlayOn) {
+    turnOffFileAudio();
+    return;
+  }
 
-function playFile() {
-    if (filePlayOn) {
-      turnOffFileAudio();
-      return;
-    }
-
-    if (micOn) {
+  if (micOn) {
     turnOffMicAudio();
-    }
+  }
 
-    sourceNode = context.createBufferSource();
-
-    sourceNode.buffer = demo_buffer;
-    sourceNode.connect(context.destination);
-    sourceNode.start(0);
-
+  sourceNode = context.createBufferSource();
+  sourceNode.buffer = demo ? demoBuffer : fileBuffer;
+  sourceNode.connect(context.destination);
+  sourceNode.start(0);
   sourceNode.connect(analyser);
 
   // visualize audio animation
-  animation_id = requestAnimationFrame(animation_function, context.sampleRate/analyser.fftSize);
+  animationId = requestAnimationFrame(animate, context.sampleRate/analyser.fftSize);
 
   filePlayOn = true;
   
-  var demoAudio = document.getElementById("demoAudio");
-  demoAudio.innerHTML = 'Demo Audio Stop'
+  if (demo) {
+    $demoButton.textContent = 'Demo Audio Stop';
+  } else {
+    $fileButton.textContent = 'File Audio Stop';
+  }
 }
 
-
 function turnOffMicAudio() {
-  var mic = document.getElementById("micInput");    
-  mic.innerHTML = 'Mic On'
+  $micButton.textContent = 'Mic On';
   mediaSourceNode = null;
   micOn = false;
 
@@ -207,58 +210,61 @@ function turnOffMicAudio() {
 }
 
 function turnOffFileAudio() {
-  var demoAudio = document.getElementById("demoAudio");
-  demoAudio.innerHTML = 'Demo Audio Play'
+  $demoButton.textContent = 'Demo Audio Play';
+  $fileButton.textContent = 'File Audio Play';
   sourceNode.stop(0);
-    sourceNode = null;
-    filePlayOn = false;
+  sourceNode = null;
+  filePlayOn = false;
 
   stopAnimation();
 }
 
 function stopAnimation() { 
+  $body.style.backgroundColor = 'white';
+  $container.style.backgroundColor = 'white';
+  $box.style.backgroundColor = 'white';
   $box.style.opacity = 0;
   prevTime = null;
-  window.cancelAnimationFrame(animation_id);
+  window.cancelAnimationFrame(animationId);
 }
-function calc_octaveband(input_array) {
-  var lower_freqs = [22, 44, 88, 177, 355, 710, 1420, 2840, 5680, 11360];
-  var upper_freqs = [44, 88, 177, 355, 710, 1420, 2840, 5680, 11360, 22720];
-  var center_freqs = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
+
+function calcOctaveband(inputArray) {
+  const lowerFreqs = [22, 44, 88, 177, 355, 710, 1420, 2840, 5680, 11360];
+  const upperFreqs = [44, 88, 177, 355, 710, 1420, 2840, 5680, 11360, 22720];
+  const centerFreqs = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000];
 
   // compute FFT power first 
-  var fft_power = new Array(input_array.length);
-  for (var i = 0; i < input_array.length; i++) {
-    fft_power[i] = Math.pow(10.0, input_array[i]/10.0);
+  const fftPower = new Array(inputArray.length);
+  for (let i = 0; i < inputArray.length; i++) {
+    fftPower[i] = Math.pow(10.0, inputArray[i]/10.0);
   }
 
-  var band_power = new Array(center_freqs.length);
+  const bandPower = new Array(centerFreqs.length);
 
-  for (var i = 0; i < center_freqs.length; i++) {
-    var lower_bin = Math.floor(lower_freqs[i]/22050.0*input_array.length);
-    var upper_bin = Math.floor(upper_freqs[i]/22050.0*input_array.length);
-    var center_bin = Math.ceil(center_freqs[i]/22050.0*input_array.length);
+  for (let i = 0; i < centerFreqs.length; i++) {
+    const lowerBin = Math.floor(lowerFreqs[i] / 22050.0 * inputArray.length);
+    let upperBin = Math.floor(upperFreqs[i] / 22050.0 * inputArray.length);
+    // const center_bin = Math.ceil(centerFreqs[i] / 22050.0 * inputArray.length);
 
-    if (upper_bin >= fft_power.length) {
-      upper_bin = fft_power.length-1;
+    if (upperBin >= fftPower.length) {
+      upperBin = fftPower.length - 1;
     }
 
-    band_power[i] = 0;
-    for (var j=lower_bin; j<upper_bin; j++ ) {
-      band_power[i] = band_power[i] + fft_power[j];
+    bandPower[i] = 0;
+    for (let j = lowerBin; j< upperBin; j++ ) {
+      bandPower[i] = bandPower[i] + fftPower[j];
     }
   }
 
-  var band_level_db = new Array(band_power.length);
+  const bandLevelDbs = new Array(bandPower.length);
 
-  for (var i = 0; i < band_level_db.length; i++) {
-    band_level_db[i] = 10.0*Math.log10(band_power[i]);
+  for (let i = 0; i < bandLevelDbs.length; i++) {
+    bandLevelDbs[i] = 10.0 * Math.log10(bandPower[i]);
 
-    if (band_level_db[i] < SOUND_METER_MIN_LEVEL) {
-      band_level_db[i] = SOUND_METER_MIN_LEVEL;
+    if (bandLevelDbs[i] < MIN_DB) {
+      bandLevelDbs[i] = MIN_DB;
     }
-
   }
 
-  return band_level_db;
+  return bandLevelDbs;
 }
